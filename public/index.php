1fce08069b94e4f86a862e2271d6455422bbfedc
pczerox <pczeroxstore@gmail.com>
2023-10-12 03:56:30 -0500

    10 - Master project routing

--- public/index.php
+++ public/index.php
@@ -5,27 +5,32 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
-  body {
-    margin: 0;
-    color: #fff;
-    background-color: #000;
-  }
+    body {
+      margin: 0;
+      color: #fff;
+      background-color: #000;
+    }
+
+    .container {
+      max-width: 764px;
+      margin-inline: auto;
+    }
   </style>
   <title>PHPiggy</title>
 </head>
 
 <body>
-  <h1>PHPiggy</h1>
-  <?php
-    var_dump(__DIR__);
-
-    echo "<br>";
+  <div class="container">
+    <h1>PHPiggy</h1>
+    <?php
+    include_once __DIR__ . "/../src/App/functions.php";
 
     $app = include_once __DIR__ . "/../src/App/bootstrap.php";
 
     $app->run();
 
-  ?>
+    ?>
+  </div>
 </body>
 
 </html>
